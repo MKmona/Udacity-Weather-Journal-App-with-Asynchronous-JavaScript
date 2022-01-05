78efe9abfe1de6ee00955677a84d1241611b0153
Mona Khalaf <m__k24@hotmail.com>
2022-01-05 15:50:01 +0100

    This is the weather journal app

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Udacity-Weather-Journal-App-with-Asynchronous-JavaScript
+# Weather Journal App with Asynchronous JavaScript  
 This project is an asynchronous web app that uses Web API and user data to dynamically update the UI in a Weather Journal application.
+
+## Table of Contents
+
+* [website](#Folder) 
+    *   [app.js](#Javascript)
+    *   [index.html](#HTML)
+    *   [styles.css](#CSS)
+*  [README.md](#README.md)
+* [server.js](#Javascript)
+
+
+## Instructions
+
+- Run the project by writing the following command line in the terminal:
+    node server.js
+- You may need to install the three packages that I used, which are (express, body-parser, and cors). From the terminal write the following command line:
+    npm install Package-name
+- Open the index.html in a web browser. In the browser write localhost:3000. That's my port number.
+- In the browser, you should enter a valid zipcode for a city and your feeling. Then click on the button named "Generate".
---
+++ website/app.js
@@ -0,0 +1,84 @@
+/* Global Variables */
+let baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = '&appid=5e77ece155b961c08f7d9ec54ffb9784';
+
+// Create a new date instance dynamically with JS
+let dateObject = new Date();
+
+// Async GET remote API
+const getData = async(zip)=>{
+    const request = await fetch(baseUrl+zip+apiKey);
+    try{
+        const response = await request.json();
+        return response;
+    }
+    catch(error){
+        console.log("error", error);
+    }
+}
+
+// Async POST local API
+const postData = async(url, data={})=>{
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        }, 
+        body: JSON.stringify(data),
+    });
+    try{
+        const newData = await response.json();
+        return newData;
+    }
+    catch(error){
+        console.log("error", error);
+    }
+}
+
+
+//Update function
+const updateUI = async ()=>{
+    const request = await fetch('/getData');
+    try{
+        const allData = await request.json();
+        document.getElementById('date').innerHTML = allData[allData.length-1].date;
+        document.getElementById('temp').innerHTML = allData[allData.length-1].temperature;
+        document.getElementById('content').innerHTML = allData[allData.length-1].content;
+    }catch(error){
+        console.log("error", error);
+    }
+}
+
+//Click Event
+const btn = document.getElementById('generate');
+btn.addEventListener('click', takeAction);
+
+function takeAction(e){
+    const zipCode =  document.getElementById('zip').value;
+    const feelings =  document.getElementById('feelings').value;
+    const newDate = dateObject.getMonth()+1+'.'+ dateObject.getDate()+'.'+ dateObject.getFullYear();
+
+    if(zipCode == ''){
+        alert("Please Enter the Zip Code");
+    }
+    else{
+        getData(zipCode).then(function(data){
+                postData('/sendData', {
+                    temperature: data.main.temp, 
+                    date: newDate, 
+                    content: feelings
+                });}).then(function(data){
+                    updateUI()
+                });
+    }
+}
+
+    
+
+    
+
+  
+
+
+
